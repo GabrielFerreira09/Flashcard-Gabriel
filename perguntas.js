b489c61341f377c95cb19c3a600f29d3db9ecc0b
Gabriel Ferreira <169099072+GabrielFerreira09@users.noreply.github.com>
2024-11-21 14:03:40 -0300

    meu projeto de perguntas

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Qual é o time com mais Copas Libertadores da América?',
+    'O Club Atlético Independiente é o maior campeão da Libertadores com 7 títulos da competição'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Indonésia?',
+    'A capital da Indonésia é Jacarta'
+)
+
+criaCartao(
+    'História',
+    'Em que ano começou e em que ano terminou a Segunda Guerra Mundial?',
+    'A Segunda Guerra Mundial começou no ano de 1939 e terminou no ano de 1945'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual a data de fundação do Sport Club Corinthians Paulista?',
+    'O Sport Club Corinthians Paulista foi fundado em 1 de setembro de 1910'
+)
